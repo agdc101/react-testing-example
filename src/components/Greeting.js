c36ab7dfbc97fed99275da656cccd362601867aa
agdc101 <agdc101@gmail.com>
2024-11-04 19:54:11 +0000

    feat: new test added to demo user interaction on btn

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,8 +1,18 @@
+import {useState} from "react";
+
 const Greeting = () => {
+    const [changeText, setChangeText] = useState(false);
+
+    const changeTextHandler = () => {
+        setChangeText(!changeText);
+    }
+
     return (
         <div className="greeting">
             <h2>Hello World!</h2>
-            <p>Good to be here</p>
+            {!changeText && <p>Good to be here</p>}
+            {changeText && <p>Changed!</p>}
+            <button onClick={changeTextHandler}>change here</button>
         </div>
     )
 }
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,11 +1,38 @@
+
 import {render, screen} from '@testing-library/react';
+import userEvent from "@testing-library/user-event";
 import Greeting from './Greeting';
 
 describe('Greeting component', () => {
-    test('renders correctly', () => {
+    test('renders hello world text', () => {
         render(<Greeting />);
 
         const helloWorldElement = screen.getByText("Hello World!");
         expect(helloWorldElement).toBeInTheDocument();
+    })
+
+    test('renders good to be here if button NOT clicked', () => {
+        render(<Greeting />);
+
+        const paragraphElement = screen.getByText("good to be here", {exact: false});
+        expect(paragraphElement).toBeInTheDocument();
+    })
+
+    test('renders changed! if button is clicked', () => {
+        render(<Greeting />);
+
+        userEvent.click(screen.getByRole('button', { name: /change/i }));
+
+        const paragraphElement = screen.getByText("changed!", {exact: false});
+        expect(paragraphElement).toBeInTheDocument();
+    })
+
+    test('does not render "good to be here" if button was clicked', () => {
+        render(<Greeting />);
+
+        userEvent.click(screen.getByRole('button', { name: /change/i }));
+
+        const paragraphElement = screen.queryByText("good to be here", {exact: false});
+        expect(paragraphElement).toBeNull();
     })
 })
